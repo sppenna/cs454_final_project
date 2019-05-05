57d32aafa666ce196e678331b93bcd5a748b66a6
sppenna <leveetoo@gmail.com>
2019-05-05 14:43:47 -0700

    base

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def BuildAdjList(state, queue, parent, visited, adjList):
             visited[adj[0]] = 1
     return state, queue, parent, visited, adjList
 
+#BFS of the adjacent nodes
 def MinString(state, queue, parent, visited, inputList, k):
     #initialize empty string
     out = ""
@@ -26,29 +27,41 @@ def MinString(state, queue, parent, visited, inputList, k):
     while True:
         if len(queue) <= 0:
             break
-        
+        print("queue: ", queue)
         searchNext = queue.pop()
         state = searchNext[0]
-        
-        if float(searchNext[0]) == float(searchNext[1]):
-           print("test") 
-        if state != 0:
-            adjList = map(lambda x: ((int(state) * 10 + int(x)) % int(k), x), inputList)
-            state, queue, parent, visited, adjList = BuildAdjList(searchNext, queue, parent, visited, adjList)
-        '''
-        else:
+        #print("searchNext[0]: ", searchNext[0], " searchNext[1]: ", searchNext[1])
+        if state is 0:
+            
             #reached an accepting state append the queue to output string until you reach -1
             while searchNext[0] != -1:
                 out += str(searchNext[1])
                 searchNext = parent[searchNext[0]]
-            #reverse the string and return 
+            #reverse the string and return
             return out[::-1]
-        '''
+
+        else:
+            adjList = map(lambda x: ((int(state) * 10 + int(x)) % int(k), x), inputList)
+            state, queue, parent, visited, adjList = BuildAdjList(searchNext, queue, parent, visited, adjList)
+        
     return "No solution exists"
 
+ 
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s): 
+    rev = reverse(s) 
+  
+    if (s == rev): 
+        return True
+    return False
+
 def main():
-    print("***Find The Shortest Palindrome***")
     while True:
+        print("************************************")
+        print("*** Find The Shortest Palindrome ***")
+        print("************************************")
         userKInput = int(input("Enter the number 'k' to find shortest multiple of: "))
         userNInput = input("Enter Numbers Permitted (separated by spaces): ")
         inputList = []
