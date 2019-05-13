bedaa93e2477033ed1eaef85270c128319459d90
sppenna <leveetoo@gmail.com>
2019-05-13 07:16:12 -0700

    using tyler's project1

--- main.py
+++ main.py
@@ -1,72 +1,84 @@
-#Finds a multiple of k with a list n of permitted digits.
-def SmallestMultiple(k, inputList):
-    state = (-1, "")
-    queue = []
-    parent = {}
-    visited = {}
-    #list of adjacent nodes
-    adjList = map(lambda x: ((int(x)) % int(k), x), inputList)
-    state, queue, parent, visited, adjList = BuildAdjList(state, queue, parent, visited, adjList)
-    return MinString(state, queue, parent, visited, inputList, k)
-
-#build base adjacency list
-def BuildAdjList(state, queue, parent, visited, adjList):
-    for adj in adjList:
-        if adj[0] not in visited:
-            queue.insert(0, adj)
-            parent[adj[0]] = state
-            visited[adj[0]] = 1
-    return state, queue, parent, visited, adjList
-
-#BFS of the adjacent nodes
-def MinString(state, queue, parent, visited, inputList, k):
-    #initialize empty string
-    out = ""
-    #Begin BFS
-    #go through queue of adjacent possible moves.
-    while True:
-        if len(queue) <= 0:
-            break
-        print("queue: ", queue)
-        searchNext = queue.pop()
-        state = searchNext[0]
-        #print("searchNext[0]: ", searchNext[0], " searchNext[1]: ", searchNext[1])
-        if state is 0:
-            
-            #reached an accepting state append the queue to output string until you reach -1
-            while searchNext[0] != -1:
-                out += str(searchNext[1])
-                searchNext = parent[searchNext[0]]
-            #reverse the string and return
-            return out[::-1]
-
-        else:
-            adjList = map(lambda x: ((int(state) * 10 + int(x)) % int(k), x), inputList)
-            state, queue, parent, visited, adjList = BuildAdjList(searchNext, queue, parent, visited, adjList)
-        
-    return "No solution exists"
-
- 
-def reverse(s): 
-    return s[::-1] 
-  
-def isPalindrome(s): 
-    rev = reverse(s) 
-  
-    if (s == rev): 
-        return True
-    return False
+import sys
+import time 
 
 def main():
+
+   # task = input("\n")
+    print("*************************************************")
+    print("***** Find Number of Palindromes of size N ******")
+    print("*********** Which are Multiples of K ************")
+    print("*************************************************")
     while True:
-        print("************************************")
-        print("*** Find The Shortest Palindrome ***")
-        print("************************************")
-        userKInput = int(input("Enter the number 'k' to find shortest multiple of: "))
-        userNInput = input("Enter Numbers Permitted (separated by spaces): ")
-        inputList = []
-        inputList = userNInput.split(' ')
-        out = SmallestMultiple(userKInput, inputList)
-        print("Output: " + out + '\n')
-    print('\n' + "Closing the Program...")
+        # Do problem 2
+        k = int(input("Enter value for k in the range [1, 99999]: "))
+        alphaString = input("Enter alphabet symbols separated by space (integers 1 - 9): ")
+        alphabet = []
+        for i in alphaString.split():
+            alphabet.append(int(i))
+        if k < 1 or k > 99999:
+            print("k is out of range")
+        else:
+            #print("Performing BFS on the DFA... (this may take a while, please be patient)")
+            time.sleep(0.1)
+            solution = shortestAcceptedString(k, alphabet, k)
+            print(solution)
+
+def dfaDelta(initialState, inputSymbol, k):
+    return (10 * initialState + inputSymbol) % k
+
+
+# noinspection SpellCheckingInspection
+def shortestAcceptedString(k, alphabet, startState):
+    # function to solve problem 2
+    # finds string of shortest length that is accepted by the DFA
+
+    retVals = bfs(k, alphabet, startState)
+
+    print("Answer: ")
+
+    if not retVals:
+        return "No solution"
+
+    currentstate = retVals[0]
+    nextstate = retVals[1]
+    parent = retVals[2]
+    label = retVals[3]
+    output = ""
+    while nextstate != startState:
+        output += str(label[nextstate])
+        nextstate = parent[nextstate]
+    return output[::-1]
+
+
+def bfs(k, alphabet, startState):
+    queue = []
+    # visited is an array where each index represents a state, and visited[i] gives true or false
+    # based on whether state i has been visited
+    # initialize
+    visited = [startState]
+    parent = [-1] * (k + 1)
+    label = [-1] * (k + 1)
+
+    queue.append(startState)
+    nextstate = -1
+    currentstate = -1
+
+    while queue:
+        currentstate = queue.pop(0)
+        for c in alphabet:
+            nextstate = dfaDelta(currentstate, c, k)
+            if nextstate == 0:  # accepting state reached
+                parent[nextstate] = currentstate
+                label[nextstate] = c
+                returnVals = [currentstate, nextstate, parent, label]
+                return returnVals
+            if nextstate not in visited:
+                visited.append(nextstate)
+                parent[nextstate] = currentstate
+                label[nextstate] = c
+                queue.append(nextstate)
+    if nextstate != 0:
+        return False
+
+
 main()
