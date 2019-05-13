5b96367eeadea6ac6e43f4e3f4ee9d8e4bb6dc3f
sppenna <leveetoo@gmail.com>
2019-05-13 07:36:09 -0700

    cleaning up for final project

--- main.py
+++ main.py
@@ -4,31 +4,37 @@ import time
 def main():
 
    # task = input("\n")
-    print("*************************************************")
-    print("***** Find Number of Palindromes of size N ******")
-    print("*********** Which are Multiples of K ************")
-    print("*************************************************")
+    print('\n')
+    print("**************************************************")
+    print("***** Find Number of Palindromes of size: M ******")
+    print("*********** Which are Multiples of: N ************")
+    print("**************** Using Digits: D *****************")
+    print("**************************************************")
+    print('\n')
     while True:
         # Do problem 2
-        k = int(input("Enter value for k in the range [1, 99999]: "))
-        alphaString = input("Enter alphabet symbols separated by space (integers 1 - 9): ")
-        alphabet = []
-        for i in alphaString.split():
-            alphabet.append(int(i))
-        if k < 1 or k > 99999:
+        n = int(input("Enter value for N in the range (1 - 99,999): "))
+        digitString = input("Enter digit list separated by space (integers 1 - 9): ")
+        m = int(input("Enter the size of the palindrome (M): "))
+        digitSet = []
+        for i in digitString.split():
+            digitSet.append(int(i))
+        if n < 1 or n > 99999:
             print("k is out of range")
         else:
             #print("Performing BFS on the DFA... (this may take a while, please be patient)")
             time.sleep(0.1)
-            solution = shortestAcceptedString(k, alphabet, k)
-            print(solution)
+            print('\n')
+            palindromes = shortestAcceptedString(n, digitSet, n, m)
+            print("The number of palindromes of size M: ", palindromes)
+            print('\n')
 
 def dfaDelta(initialState, inputSymbol, k):
     return (10 * initialState + inputSymbol) % k
 
 
 # noinspection SpellCheckingInspection
-def shortestAcceptedString(k, alphabet, startState):
+def shortestAcceptedString(k, alphabet, startState, m):
     # function to solve problem 2
     # finds string of shortest length that is accepted by the DFA
 
