28064b454be895ae2ba2bb1f3b3356a160ab31df
sppenna <leveetoo@gmail.com>
2019-05-14 08:17:59 -0700

    issue with generating M: state 0 need to be able to be transitioned off of

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     print('\n')
     print("**************************************************")
     print("***** Find Number of Palindromes of size: M ******")
-    print("*********** Which are Multiples of: N ************")
+    print("*********** Which are Multiples of: K ************")
     print("**************** Using Digits: D *****************")
     print("**************************************************")
     print('\n')
@@ -21,7 +21,7 @@ def main():
     while True:
         print("**************************************************")
         # Get values for n, m, d
-        n = int(input("Enter value for N in the range (1 - 99,999): "))
+        k = int(input("Enter value for K in the range (1 - 99,999): "))
         digitString = "1 2 3 4 5 6 7 8 9"
         #digitString = input("Enter digit list (D) separated by space (integers 1 - 9): ")
         m = int(input("Enter the size of the palindrome (M): "))
@@ -31,13 +31,13 @@ def main():
         for i in digitString.split():
             digitSet.append(int(i))
         #check n's size
-        if n < 1 or n > 99999:
+        if k < 1 or k > 99999:
             print("k is out of range")
         else:
             time.sleep(0.1)
             print('\n')
             #palindromes = numPalindromes(n, digitSet, n, m)
-            numPalindromes(n, digitSet, n, m)
+            NumPalindromes(k, digitSet, k, m)
 
             #if palindromes is 0:
             #    print("No solution")
@@ -45,21 +45,18 @@ def main():
                 #print("The number of palindromes of size M: ", palindromes)
 
 
-def dfaDelta(initialState, inputSymbol, k):
+def M_Transition(initialState, inputSymbol, k):
     return (10 * initialState + inputSymbol) % k
 
 
 # noinspection SpellCheckingInspection
-def numPalindromes(k, digitSet, startState, m):
+def NumPalindromes(k, digitSet, startState, m):
     # function to solve problem 2
     # finds string of shortest length that is accepted by the DFA
 
-    retVals = generateM(k, digitSet, startState, m)
+    retVals = Generate_M(k, digitSet, startState, m)
     print(retVals)
     print("Answer: ")
-    #if not retVals:
-        #return "No solution"
-    #return retVals
     '''
     currentState = retVals[0]
     nextState = retVals[1]
@@ -72,7 +69,7 @@ def numPalindromes(k, digitSet, startState, m):
     return output[::-1]
     '''
 
-def generateM(k, digitSet, startState, m):
+def Generate_M(k, digitSet, startState, m):
     queue = []
     # visited is an array where each index represents a state, and visited[i] gives true or false
     # based on whether state i has been visited
@@ -87,40 +84,41 @@ def generateM(k, digitSet, startState, m):
     nextState    = -1
     currentState = -1
 
-    count = 0
+    count = 1
     
     while len(queue) is not 0:
-    #while count < m and len(queue) is not 0:
-        
         currentState = queue.pop(0)
-
         count+=1
+        
         for c in digitSet:
     
-            nextState = dfaDelta(currentState, c, k)
+            nextState = M_Transition(currentState, c, k)
 
             if nextState is 0:  # accepting state reached
-                
-                parent[nextState] = currentState
-                label[nextState] = c
-                returnVals = [currentState, nextState, parent, label]
-
-                while nextState != startState:
-                    output += str(label[nextState])
-                    nextState = parent[nextState]
-                print(output[::-1])
-                
-                output = ""
-                #return returnVals
-            if nextState not in visited:
                 visited.append(nextState)
                 parent[nextState] = currentState
-                label[nextState] = c
+                label[nextState]  = c
+
+                while nextState != startState:
+                    output      += str(label[nextState])
+                    nextState    = parent[nextState]
+                
+                print(output[::-1])
+                nextState = int(output[::-1])
+                queue.append(nextState)
+
+
+                output = ""
+               
+            elif nextState not in visited:
+                visited.append(nextState)
+                parent[nextState] = currentState
+                label[nextState]  = c
                 queue.append(nextState)
                 
     #return queue
     
-def generateM_Prime(k, digitSet, startState, m):
+def Generate_M_Prime(k, digitSet, startState, m):
     print("in progress")
 
 main()
