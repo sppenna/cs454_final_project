5c67b6dfcfd4cc6a654f2704a92cf011f45a6c03
sppenna <leveetoo@gmail.com>
2019-05-15 13:11:07 -0700

    test

--- main.py
+++ main.py
@@ -12,42 +12,42 @@ def main():
 
     print('\n')
     print("**************************************************")
-    print("***** Find Number of Palindromes of size: M ******")
-    print("*********** Which are Multiples of: K ************")
-    print("**************** Using Digits: D *****************")
-    print("**************************************************")
-    print('\n')
+    print( "***** Find Number of Palindromes of size: M ******" )
+    print( "*********** Which are Multiples of: K ************" )
+    print( "**************** Using Digits: D *****************" )
+    print( "**************************************************" )
+    print( '\n' )
     
     while True:
-        print("**************************************************")
+        print( "**************************************************" )
         # Get values for n, m, d
-        k = int(input("Enter value for K in the range (1 - 99,999): "))
+        k = int( input( "Enter value for K in the range (1 - 99,999): ") )
         digitString = "1 2 3 4 5 6 7 8 9"
         #digitString = input("Enter digit list (D) separated by space (integers 1 - 9): ")
-        m = int(input("Enter the size of the palindrome (M): "))
+        m = int( input("Enter the size of the palindrome (M): ") )
         digitSet = []
 
         #split digitString and store as a list
         for i in digitString.split():
-            digitSet.append(int(i))
+            digitSet.append( int(i) )
         #check n's size
         if k < 1 or k > 99999:
-            print("k is out of range")
+            print( "k is out of range" )
         else:
-            time.sleep(0.1)
-            print('\n')
+            time.sleep( 0.1 )
+            print( '\n' )
             #palindromes = numPalindromes(n, digitSet, n, m)
-            NumPalindromes(k, digitSet, k, m)
+            NumPalindromes( k, digitSet, k, m )
 
 
 def M_Transition(initialState, inputSymbol, k):
-    return (10 * initialState + inputSymbol) % k
+    return ( 10 * initialState + inputSymbol ) % k
 
 
 def NumPalindromes(k, digitSet, startState, m):
     # finds a list of viable palindromes of size m
     # calls Generate_M to do this evaluation
-    Generate_M(k, digitSet, startState, m)
+    Generate_M( k, digitSet, startState, m )
 
     print("Answer: ")
 
@@ -55,10 +55,10 @@ def NumPalindromes(k, digitSet, startState, m):
 def Adjacent(visited, parent, label, queue, c, currentState, nextState):
     # Helper function saves the nextState to the move queue and
     # saves its parent as currentState
-    visited.append(nextState)
+    visited.append( nextState )
     parent[nextState] = currentState
     label[nextState]  = c
-    queue.append(nextState)
+    queue.append( nextState )
 
     return visited, parent, label, queue
 
@@ -73,10 +73,10 @@ def Generate_M(k, digitSet, startState, m):
     output  = ""
 
     visited = [startState]
-    parent  = [-1] * (k + 1)
-    label   = [-1] * (k + 1)
+    parent  = [-1] * ( k + 1 )
+    label   = [-1] * ( k + 1 )
 
-    queue.append(startState)
+    queue.append( startState )
     nextState    = -1
     currentState = -1
 
@@ -89,7 +89,7 @@ def Generate_M(k, digitSet, startState, m):
 
         for c in digitSet:
             # generate a M transition for each digit in your digitList
-            nextState = M_Transition(currentState, c, k)
+            nextState = M_Transition( currentState, c, k )
 
             if nextState is 0:  # accepting state reached
 
@@ -97,11 +97,11 @@ def Generate_M(k, digitSet, startState, m):
                 label[nextState]  = c
 
                 while nextState != startState:
-                    output      += str(label[nextState])
+                    output      += str( label[nextState] )
                     nextState    = parent[nextState]
 
                 print(output[::-1])
-                nextState = int(output[::-1])
+                nextState = int( output[::-1] )
                 #print("visited: ", visited)
                 #print("nextState: ", nextState)
                 #print("queue: ", queue)
@@ -112,9 +112,9 @@ def Generate_M(k, digitSet, startState, m):
             elif nextState not in visited: 
                 # Adjacent saves the nextState to the queue and saves its parent
                 visited, parent, label, queue = Adjacent( visited, parent, label, queue,
-                                                c, currentState, nextState )
+                                                          c, currentState, nextState )
 
-    print(queue)
+    print( queue )
 
 
 def M_Prime_Transition(k, digitSet, startState, m):
